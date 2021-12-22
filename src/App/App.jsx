a22287d806512bd1f4333295f92b9e7163a486aa
LarysaTymoshenko <luda76@ukr.net>
2021-12-22 12:01:33 +0200

    1201

---
+++ src/App/App.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { ToastContainer } from "react-toastify";
+import ImageGallery from "../ImageGallery";
+import Modal from "../Modal";
+import ScrollUp from "../ScrollUp";
+import Searchbar from "../Searchbar";
+import Section from "../Section";
+import "./App.css";
+
+export default class App extends Component {
+  state = {
+    imageName: "",
+    showModal: false,
+    modalImg: {
+      src: "",
+      alt: "",
+    },
+  };
+
+  onFormSubmit = (imageName) => {
+    this.setState({ imageName });
+  };
+
+  toggleModal = (src, alt) => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+      modalImg: {
+        src,
+        alt,
+      },
+    }));
+  };
+
+  render() {
+    const { imageName, showModal, modalImg } = this.state;
+    return (
+      <>
+        <Section>
+          <Searchbar onSubmit={this.onFormSubmit} />
+        </Section>
+        <Section>
+          <ImageGallery imageName={imageName} openModal={this.toggleModal} />
+          <ScrollUp />
+        </Section>
+
+        {showModal && <Modal onClose={this.toggleModal} modalImg={modalImg} />}
+        <ToastContainer autoClose={3000} />
+      </>
+    );
+  }
+}
